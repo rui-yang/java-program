f059afabee2791d4880d48fca7952f59f672764a
rae <creator.ruiyang@gmail.com>
2020-10-06 19:45:07 +0200

    read from file

--- MySweetApp/src/Main.java
+++ MySweetApp/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("this is my sweet app");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File("src/test.txt"));
+        List<String> texts = new ArrayList<String>();
+
+        while(in.hasNextLine()) {
+            texts.add(in.nextLine());
+        }
+
+        for(int i = 0; i < texts.size(); i++) {
+            System.out.println("Line #" + i + ": "+ texts.get(i));
+        }
+        
+        in.close();
+
     }
 }
